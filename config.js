bd668c43e9f83bdae464f2cedf035b1b2933371c
divrawat <divrawat2001@gmail>
2024-06-03 20:13:53 +0530

    s0evs5vg

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-
 export const DOMAIN = "https://aktieprognose.in.net";
 // export const DOMAIN = "http://localhost:3000";
 
-export const APP_DESCRIPTION = 'DirmaTech';
+export const APP_DESCRIPTION = "Welcome to Aktieprognose, your go-to destination for insightful and timely information on business, stock markets, and the latest news. Whether you're a seasoned investor, an aspiring entrepreneur, or simply someone who loves staying informed, our blog offers something for everyone.";
 
 // Navbar
 export const logo = <img src="/images/train-logo.png" alt="Logo" className="h-[40px] w-[50px] mr-5 md:ml-0 ml-4 md:my-0 my-1" />
-export const APP_NAME = 'DirmaTech';
-export const NavbarName = "DirmaTech";
+export const APP_NAME = 'Aktie Prognose';
+export const NavbarName = "Aktie Prognose";
 export const navLinks = [
-  { text: 'Tailwind', href: '/categories/tailwind' },
-  { text: 'Next.js', href: '/categories/next.js' },
+  { text: 'Stock Market', href: '/categories/stock-market' },
+  { text: 'Stock Brokers', href: '/categories/stock-brokers' },
+  { text: 'Mutual Fund', href: '/categories/mutual-fund' },
 ];
 
 
