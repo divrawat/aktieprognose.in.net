95dfa4957c063b468f54b15a49538ff77a49c35c
divrawat <divrawat2001@gmail>
2024-06-03 19:17:49 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+
+export const DOMAIN = "https://dirmatech.com";
+// export const DOMAIN = "http://localhost:3000";
+
+export const APP_DESCRIPTION = 'DirmaTech';
+
+// Navbar
+export const logo = <img src="/images/train-logo.png" alt="Logo" className="h-[40px] w-[50px] mr-5 md:ml-0 ml-4 md:my-0 my-1" />
+export const APP_NAME = 'DirmaTech';
+export const NavbarName = "DirmaTech";
+export const navLinks = [
+  { text: 'Tailwind', href: '/categories/tailwind' },
+  { text: 'Next.js', href: '/categories/next.js' },
+];
+
+
+// Footer
+export const FooterLinks = [
+  { text: 'About', href: '/about' },
+  { text: 'Contact', href: '/contact' },
+  { text: 'Disclaimer', href: '/disclaimer' },
+  { text: 'Privacy Policy', href: '/privacy-policy' }
+];
