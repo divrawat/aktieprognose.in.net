8f62583d67feffb829104ac7c5ca5fe575276ec1
divrawat <divrawat2001@gmail>
2024-06-05 14:50:36 +0530

    sg0vsd5gv

--- pages/api/blog/allblogs.js
+++ pages/api/blog/allblogs.js
@@ -13,7 +13,7 @@ const handler = async (req, res) => {
         const query = { $and: [{ title: { $regex: search, $options: 'i' } }, { status: { $ne: 'Draft' } }] };
         const skip = (page - 1) * perPage;
         const data = await Blog.find(query)
-            .populate({ path: 'postedBy', model: User, select: 'name username' })
+            .populate({ path: 'postedBy', model: User, select: 'name username' }).select('title slug postedBy date')
             .sort({ date: -1 }).skip(skip).limit(perPage).exec();
         res.json({
             status: true,
